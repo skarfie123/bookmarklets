e7fb54447b494c1acb47c48fefe5642ec24d7dad
skarfie123 <rahulpai@rocketmail.com>
2023-10-08 12:59:10 +0100

    path to folder as argument

--- bookmarklets/__main__.py
+++ bookmarklets/__main__.py
@@ -1,6 +1,7 @@
-import os
+import pathlib
 import urllib.parse
 import webbrowser
+from pathlib import Path
 from typing import Annotated
 
 import typer
@@ -12,34 +13,53 @@ cli = typer.Typer(
     context_settings={"help_option_names": ["-h", "--help"]}, no_args_is_help=True
 )
 
-app = FastAPI()
+Folder = Annotated[Path, typer.Argument(help="Folder to save the bookmarklets")]
 
 
-def _get_code() -> dict[str, str]:
+def _get_code(folder: Path) -> dict[str, str]:
     code = {}
-    src_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), "examples")
-    code_files = sorted(file for file in os.listdir(src_folder) if file.endswith(".js"))
+    code_files = sorted(file for file in folder.iterdir() if file.suffix == ".js")
     for file in code_files:
-        with open(os.path.join(src_folder, file), encoding="utf-8") as f:
-            code[file[:-3]] = f.read()
+        with open(file, encoding="utf-8") as f:
+            code[file.name[:-3]] = f.read()
     return code
 
 
-def _get_bookmarklets():
-    all_code = _get_code()
+def _get_bookmarklets(folder: Path):
+    all_code = _get_code(folder)
     return {
         name: f"javascript:(() => {{{urllib.parse.quote(code)}}})()"
         for name, code in all_code.items()
     }
 
 
-@app.get("/", response_class=HTMLResponse)
-def root():
-    links = "\n".join(
-        f'<p><a class="bookmarklet" href="{bookmarklet}">{name}</a></p>'
-        for name, bookmarklet in _get_bookmarklets().items()
-    )
-    html_response = f"""<html>
+@cli.command(help="Start the bookmarklet server with links to all the bookmarklets")
+def server(
+    folder: Folder = pathlib.Path.cwd(),
+    open_brower: Annotated[
+        bool, typer.Option("-o", "--open", help="Open the web page in the browser")
+    ] = False,
+    port: Annotated[
+        int,
+        typer.Option(
+            "-p", "--port", help="Port to run the server on", min=1, max=65535
+        ),
+    ] = 8000,
+    public: Annotated[bool, typer.Option(help="Make the server public")] = False,
+):
+    typer.echo("Opening web page with all bookmarklets")
+    typer.echo("Drag the bookmarklets to your bookmarks bar to install them")
+    typer.echo("Press CTRL+C to stop the server")
+
+    app = FastAPI(folder=folder)
+
+    @app.get("/", response_class=HTMLResponse)
+    def root():
+        links = "\n".join(
+            f'<p><a class="bookmarklet" href="{bookmarklet}">{name}</a></p>'
+            for name, bookmarklet in _get_bookmarklets(folder).items()
+        )
+        html_response = f"""<html>
     <head>
         <title>Bookmarklets</title>
         <style>
@@ -73,49 +93,27 @@ def root():
         </div>
     </body>
 </html>"""
-    return html_response
-
-
-@cli.command(help="Start the bookmarklet server with links to all the bookmarklets")
-def server(
-    open_brower: Annotated[
-        bool, typer.Option("-o", "--open", help="Open the web page in the browser")
-    ] = False,
-    port: Annotated[
-        int,
-        typer.Option(
-            "-p", "--port", help="Port to run the server on", min=1, max=65535
-        ),
-    ] = 8000,
-    public: Annotated[bool, typer.Option(help="Make the server public")] = False,
-):
-    typer.echo("Opening web page with all bookmarklets")
-    typer.echo("Drag the bookmarklets to your bookmarks bar to install them")
-    typer.echo("Press CTRL+C to stop the server")
+        return html_response
 
     if open_brower:
         webbrowser.open_new_tab(f"http://localhost:{port}")
 
     host = "0.0.0.0" if public else "127.0.0.1"  # nosec: B104
-    uvicorn.run(
-        "bookmarklets.__main__:app",
-        host=host,
-        port=port,
-        reload=True,
-    )
+    uvicorn.run(app, host=host, port=port)
 
 
 @cli.command(help="Generate bookmarks.html to import into browsers")
 def html(
+    folder: Folder = pathlib.Path.cwd(),
     output: Annotated[
         str, typer.Option("-o", "--output", help="Output file name")
-    ] = "bookmarks.html"
+    ] = "bookmarks.html",
 ):
     typer.echo(f"Writing {output}")
     with open(output, "w", encoding="utf-8") as f:
         links = "\n        ".join(
             f'<DT><A HREF="{bookmarklet}">{name}</A>'
-            for name, bookmarklet in _get_bookmarklets().items()
+            for name, bookmarklet in _get_bookmarklets(folder).items()
         )
         # write a bookmarks.html file for importing into browsers
         # https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa753582(v=vs.85)
